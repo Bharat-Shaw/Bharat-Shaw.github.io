56ffe7f48a21c11ca5cbb40104fb3dcd9ac89a2c
Bharat <bharat.teachief0171@gmail.com>
2023-06-17 21:17:36 +0530

    new changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,7 @@ export default function About() {
           </p>
           <br />
         </div>
-        <a onClick={handleDownload} rel="noreferrer" href={resume} download={"Bharat-Durge-Resume"} id="resume-button-2" className="contact-button resume-button second-btn">Resume</a>
+        <a onClick={handleDownload} rel="noreferrer" href={resume} download={"Bharat-Resume"} id="resume-button-2" className="contact-button resume-button second-btn">Resume</a>
       </div>
     </section>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar({ handleClick, boolean }) {
           </a>
         </h1>
         <div className="ham-container">
-          <a rel="noreferrer" href={resume} onClick={handleDownload} download={'Bharat-Durge-Resume'} className="contact-button resume-button first-btn">
+          <a rel="noreferrer" href={resume} onClick={handleDownload} download={'Bharat-Resume'} className="contact-button resume-button first-btn">
             Resume
           </a>
           <span className="ham">
@@ -51,7 +51,7 @@ export default function Navbar({ handleClick, boolean }) {
           </a>
         </li>
       </ul>
-      <a href={resume} onClick={handleDownload} rel="noreferrer" download={'Bharat-Durge-Resume'} id="resume-button-1" className="contact-button nav-link resume resume-button second-btn">Resume</a>
+      <a href={resume} onClick={handleDownload} rel="noreferrer" download={'Bharat-Resume'} id="resume-button-1" className="contact-button nav-link resume resume-button second-btn">Resume</a>
     </nav>
   );
 }
